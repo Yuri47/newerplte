708d5b37a26d2fe64e0ff6241430d661eb772bc3
Yuri47 <yuri.alexs@gmail.com>
2016-10-13 12:15:57 -0300

    second commit from linux

---
+++ resources/views/os/listos.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('htmlheader_title')
+  
+@endsection
+
+
+@section('main-content')
+ 
+<h1>Lista de OS</h1>
+
+<os-list></os-list>
+
+<script src="/js/main.js"></script>
+
+@endsection
